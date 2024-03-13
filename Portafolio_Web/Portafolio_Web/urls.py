89c7bad4d7d51c3a0bf66aeda4794572d41ccb62
Felipe Calvo Béhèran <felipecalvo6@gmail.com>
2024-03-13 03:11:43 -0300

    test power automate

--- Portafolio_Web/Portafolio_Web/urls.py
+++ Portafolio_Web/Portafolio_Web/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from Portafolio_Web.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('b1/', b1)
 ]
--- Portafolio_Web/Portafolio_Web/views.py
+++ Portafolio_Web/Portafolio_Web/views.py
@@ -1,2 +1,20 @@
 from django.http import HttpResponse
+import datetime
 
+def b1(request):
+    return HttpResponse("Bienvenido a mi portafolio")
+
+def fecha(request):
+    hoy = datetime.datetime.now()
+    respuesta = f"""
+    <html>
+    <h1> Hoy es {hoy} </h1>
+    </html>
+    """
+    
+    return HttpResponse(respuesta)
+
+
+def bienvenido_template(request):
+    
+    miHml = open("")
